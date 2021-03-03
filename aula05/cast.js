a7d624a70b4ab8b3c71cbe8c8137f3fe263e3a3a
Dauan Ghisleni Zolinger <dauanghislenizolinger@hotmail.com>
2021-03-03 15:02:20 -0300

    Desafio da aula 8

---
+++ aula05/cast.js
@@ -0,0 +1,7 @@
+let contato = 55999920277;
+
+contato= Number(contato);
+console.log(typeof contato);
+
+contato= String(contato);
+console.log(typeof contato);
---
+++ aula05/types.js
@@ -0,0 +1,11 @@
+let nome = "Dauan";
+let idade = 21;
+let cursando = true;
+let pendências = null;
+let obs;
+
+console.log(nome);
+console.log(idade);
+console.log(cursando);
+console.log(pendências);
+console.log(obs);
