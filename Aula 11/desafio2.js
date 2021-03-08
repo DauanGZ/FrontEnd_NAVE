c30479648bc9df56d0d2a8ec788956aa8b0e4619
Dauan Ghisleni Zolinger <dauanghislenizolinger@hotmail.com>
2021-03-08 15:59:06 -0300

    Desafio-array da aula 11

---
+++ Aula 11/desafio2.js
@@ -0,0 +1,33 @@
+// Desafio-array da aula 11
+
+const students = [
+    {id: 1, name: 'John Doe', age: 23, scholarship: false},
+    {id: 2, name: 'Margoe Rose', age: 19, scholarship: true},
+    {id: 3, name: 'Kayle', age: 21, scholarship: true}
+];
+
+// 1.
+
+students.forEach((estudante) => {
+  console.log(estudante.name);
+});
+
+// 2.
+
+students.forEach((estudante) => {
+  if(estudante.age > 20){
+    console.log(estudante.name); 
+  }
+});
+
+// 3.
+
+var bolsistas = [];
+
+students.forEach((estudante, posicao) => {
+  if (estudante.scholarship == true) {
+    bolsistas.push(students.slice(posicao, posicao + 1));
+  }
+});
+
+console.log(bolsistas);
