6506a5d992daf6ac43c7158a315c86a92978c289
Dauan Ghisleni Zolinger <dauanghislenizolinger@hotmail.com>
2021-04-02 18:27:37 -0300

    Desafio da aula 24

---
+++ aula24/src/services/posts.js
@@ -0,0 +1,13 @@
+import client from "axios";
+
+const api = "https://jsonplaceholder.typicode.com"; // Requisições observadar no network!
+
+export const getPostsList = () => client.get(api + "/posts");
+
+export const createPost = (data) => client.post(api + "/posts", data);
+
+export const getPost = (id) => client.get(api + "/posts/" + id);
+
+export const putPost = (data) => client.put(api + "/posts/" + data.id, data.body);
+
+export const deletePost = (id) => client.delete(api + "/posts/" + id);
